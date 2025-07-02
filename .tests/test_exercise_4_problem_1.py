fae033dc88be1b711f09b7e539b384103c552dd1
mikkokangasmaa <mikko.kangasmaa@gmail.com>
2025-07-02 10:11:59 +0300

    added tests

---
+++ .tests/test_exercise_4_problem_1.py
@@ -0,0 +1,65 @@
+from points_decorator import points
+import inspect
+import os
+import pandas as pd
+import geopandas as gpd
+import pyproj
+
+class TestProblem1:
+
+    @points(1, "Problem 1, Part 1: Did you import the grid data?")
+    def test_problem_1_part_1(self, problem1):
+        section_data, namespace = problem1
+        section = "Part 1"
+        variables = section_data[section]['variables']
+
+        assert isinstance(variables['grid'], pd.DataFrame)
+
+
+    @points(1, "Problem 1, Part 2: Did you join the travel time data into the grid cells, using the correct column names?")
+    def test_problem_1_part_2_columns(self, problem1):
+        section_data, namespace = problem1
+        section = "Part 2"
+        variables = section_data[section]['variables']
+    
+        required_columns = ['pt_r_t_Itis', 'car_r_t_Itis', 'pt_r_t_Myyrmanni', 'car_r_t_Myyrmanni', 'geometry']
+
+        assert all(column in variables['grid'].columns for column in required_columns)
+        
+
+    @points(1, "Problem 1, Part 2: Did you remove the NaN values from the travel time data?")
+    def test_problem_1_part_2_nan(self, problem1):
+        section_data, namespace = problem1
+        section = "Part 2"
+        variables = section_data[section]['variables']
+        grid = variables['grid']
+        for shopping_centre in ("Itis", "Myyrmanni"):
+            for column in ("car_r_t", "pt_r_t"):
+                assert -1 not in grid[f"{column}_{shopping_centre}"]
+
+
+    @points(1, "Problem 1, Part 3: Did you reclassify the travel time values to five-minute intervals?")
+    def test_problem_1_part_3(self, problem1):
+        section_data, namespace = problem1
+        section = "Part 3"
+        variables = section_data[section]['variables']
+        grid = variables['grid']
+
+        assert grid['pt_r_t_cl_Itis'].min() == 0.0
+        assert grid['pt_r_t_cl_Itis'].max() == 12.0
+
+        assert grid['pt_r_t_cl_Myyrmanni'].min() == 0.0
+        assert grid['pt_r_t_cl_Myyrmanni'].max() == 12.0
+
+
+    @points(1, "Problem 1, Part 4: Did you plot the data and add a title?")
+    def test_problem_1_part_3(self, problem1):
+        section_data, namespace = problem1
+        section = "Part 4"
+        variables = section_data[section]['variables']
+        source = section_data[section]['source']
+        grid = variables['grid']
+
+        assert ".plot" in source
+        assert "title" in source
+        
---
+++ .tests/test_exercise_4_problem_2.py
@@ -0,0 +1,54 @@
+from points_decorator import points
+import inspect
+import os
+import pandas as pd
+import geopandas as gpd
+import pyproj
+
+class TestProblem2:
+
+    @points(1, "Problem 2: Did you read in the travel times of each shopping centre?")
+    def test_problem_2_centres(self, problem2):
+        section_data, namespace = problem2
+        grid = namespace['grid']
+
+        expected_shopping_centres = [
+            "Itis", "Myyrmanni", "Forum", "Iso_Omena", "Ruoholahti", "Dixi", "Jumbo"
+            ]
+        
+
+        for shopping_centre in expected_shopping_centres:
+            assert f"pt_r_t_{shopping_centre}" in grid.columns  
+
+   
+    @points(1, "Problem 2: Some of the values for the nearest shopping centre seem to be incorrect.")    
+    def test_problem_2_closest_shopping_centre_value(self, problem2):
+        section_data, namespace = problem2
+        grid = namespace['grid']
+
+        # Assert that the value of "closest_shopping_centre" for the row with index 5785641 is "Myyrmanni"
+        row1 = grid.loc[5785641]
+        assert row1['dominant_shopping_centre'] == "Myyrmanni"
+
+        row2 = grid.loc[5876274]
+        assert row2['dominant_shopping_centre'] == "Itis"
+
+        row3 = grid.loc[5785641]
+        assert row3['dominant_shopping_centre'] == "Myyrmanni"
+
+    @points(1, "Problem 2: Did you save the plot as a PNG file?")
+    def test_problem_2_plot_file_exists(self,problem2):
+        section_data, namespace = problem2
+        DATA_DIRECTORY = namespace['DATA_DIRECTORY']
+
+        png_files = list(DATA_DIRECTORY.glob("*.png"))
+        assert len(png_files) > 0
+
+    @points(1, "Problem 2: Did you check that the PNG file is not empty?")
+    def test_problem_2_plot_file_not_empty(self,problem2):
+        section_data, namespace = problem2
+        DATA_DIRECTORY = namespace['DATA_DIRECTORY']
+
+        png_files = list(DATA_DIRECTORY.glob("*.png"))
+        for png_file in png_files:
+            assert png_file.stat().st_size > 0
